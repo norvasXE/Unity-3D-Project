b8a732edcd0da075be9457dd2be07a368cc31d94
tomsrevengeance <tl.littmann@gmail.com>
2023-03-09 20:31:14 +0100

    Added Camera Controller

--- My project/Assets/Scripts/CameraMoveScript.cs
+++ My project/Assets/Scripts/CameraMoveScript.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class CameraMoveScript : MonoBehaviour
 {
-    public Vector2 turn;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        turn.x += Input.GetAxis("Mouse X");
-        turn.y += Input.GetAxis("Mouse Y");
-        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        
     }
 }
--- My project/Assets/Scripts/PlayerScript.cs
+++ My project/Assets/Scripts/PlayerScript.cs
@@ -4,27 +4,81 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    float moveSpeed = 2;
+    private static float defaultSpeed = 2;
+
+    public float currentSpeed = defaultSpeed;
+
+    public float jumpMultiplier = 0.4f;
+
+    bool isJumping = false;
+
+    public Vector2 turn;
+
+    Rigidbody rb;
+
+    public GameObject camera;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rb.freezeRotation = true;
     }
 
     void Update()
     {
-        if (Input.GetKey("w")) 
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            currentSpeed *= 2;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            resetSpeed();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            rb.AddForce(new Vector3(0, 1, 0) * jumpMultiplier);
+            isJumping = true;
+            currentSpeed *= 0.25f;
+        }
+
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * Time.deltaTime * currentSpeed;
         }
         if (Input.GetKey("s"))
         {
-            transform.position += -transform.forward * Time.deltaTime * moveSpeed;
+            transform.position += -transform.forward * Time.deltaTime * currentSpeed;
         }
-        if(Input.GetKey("a"))
+        if (Input.GetKey("d"))
         {
-            //transform.position += transform.forward.Normalize(new Vector3(1, 0, 0)) * Time.deltaTime * moveSpeed;
+            transform.position += transform.right * Time.deltaTime * currentSpeed;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += -transform.right * Time.deltaTime * currentSpeed;
         }
 
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(0, turn.x, 0); //Player "body" only rotates on the y-axis
+        camera.transform.localRotation = Quaternion.Euler(-turn.y, 0, 0); //Camera "head" can rotate on x and y axis
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.name == "Ground")
+        {
+            isJumping = false;
+            resetSpeed();
+        }
+    }
+
+    private void resetSpeed()
+    {
+        currentSpeed = defaultSpeed;
     }
 }
